d8794fc1463401a0a53ada3982802a3677fdcf15
Kyle-OD <kyleodonnell@comcast.net>
2024-03-10 17:51:15 -0400

    hw1 complete, hw 2 file upload

--- homework_1/problem3.c
+++ homework_1/problem3.c
@@ -12,13 +12,14 @@ int main()
  if (pid == 0) {  
   for (i = 0; i < SIZE; i++) {  
    nums[i] *= -i; 
-   printf("CHILD: %d \n",nums[i]); /* LINE X */ 
+   printf("CHILD: %d ",nums[i]); /* LINE X */ 
   } 
  } 
  else if (pid > 0) {  
   wait(NULL); 
   for (i = 0; i < SIZE; i++) 
-   printf("PARENT: %d \n",nums[i]); /* LINE Y */ 
+   printf("PARENT: %d ",nums[i]); /* LINE Y */ 
  } 
+ printf("\n");
  return 0; 
 } 
